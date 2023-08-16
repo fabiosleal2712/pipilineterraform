44886245c82faec4cd4b2a83ce117ec6420b3f73
fabiosleal2712 <lymaleal@hotmail.com>
2023-08-16 01:03:41 -0300

    push

--- vm.tf
+++ vm.tf
@@ -9,9 +9,16 @@ resource "aws_instance" "ec2_instance" {
   key_name      = aws_key_pair.ssh-key.key_name
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ssh.id]  # Associa o grupo de segurança à instância
+  
   user_data = <<-EOF
     #!/bin/bash
-    echo "Hello, this is user data!" > user_data.txt
+    echo "sudo yum update
+    sudo yum -y install docker
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo chkconfig docker on
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose" > user_data.txt
     EOF
 
   tags = {
