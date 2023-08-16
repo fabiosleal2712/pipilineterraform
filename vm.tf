3bcd808f3b20000bdf87f3dbcbd384f605e1ca8a
fabiosleal2712 <lymaleal@hotmail.com>
2023-08-15 23:52:32 -0300

    push

--- vm.tf
+++ vm.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "ssh-key" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0af6e9042ea5a4e3e"  # Substitua pelo ID da AMI desejada
+  ami           = "ami-0344e5787e2e93144"  # Substitua pelo ID da AMI desejada
   instance_type = "t2.micro"
   key_name      = aws_key_pair.ssh-key.key_name
   subnet_id     = aws_subnet.public_subnet.id
