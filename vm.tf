334e4b9147efd2c688e72ac5472951e39b362134
fabiosleal2712 <lymaleal@hotmail.com>
2023-08-16 00:02:38 -0300

    push

--- vm.tf
+++ vm.tf
@@ -5,10 +5,9 @@ resource "aws_key_pair" "ssh-key" {
 
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-0344e5787e2e93144"  # Substitua pelo ID da AMI desejada
-  instance_type = "t2.micro"
+  instance_type = "t3a.nano"
   key_name      = aws_key_pair.ssh-key.key_name
   subnet_id     = aws_subnet.public_subnet.id
-
   vpc_security_group_ids = [aws_security_group.ssh.id]  # Associa o grupo de segurança à instância
 
   tags = {
