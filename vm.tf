0ecf8632e2b318f33ad202ca8c3cb3992bceef64
fabiosleal2712 <lymaleal@hotmail.com>
2023-08-16 01:10:35 -0300

    push

--- vm.tf
+++ vm.tf
@@ -12,13 +12,7 @@ resource "aws_instance" "ec2_instance" {
   
   user_data = <<-EOF
     #!/bin/bash
-    echo "sudo yum update
-    sudo yum -y install docker
-    sudo service docker start
-    sudo usermod -a -G docker ec2-user
-    sudo chkconfig docker on
-    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose" > user_data.txt
+    echo "Hello, this is user data!" > ,/user_data.txt
     EOF
 
   tags = {
